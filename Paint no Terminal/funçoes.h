9c2496fe87e9c9c21c0944a2cfadb8fd0f03092f
Vitor F. Carrera Miguel <62776298+VitorFCM@users.noreply.github.com>
2020-04-30 22:03:03 -0300

    Update funçoes.h

--- Paint no Terminal/funçoes.h
+++ Paint no Terminal/funçoes.h
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-int comando(char *entrada, char *comando){  // checa o inicio da entrada para determinar qual é o comando
+int comando(char *entrada, char *comando){
   int i = 0;
   while(comando[i] == entrada[i]){
     i++;
@@ -16,8 +16,8 @@ int comando(char *entrada, char *comando){  // checa o inicio da entrada para de
 }
 
 int** criar(int *alturamax, int *larguramax, char *entrada){
-  char aux[4]; //chega ate o digito 9999
-  int i = 7, j = 0; // as informações necessárias da entrada começam a partir do indice igual a 7 da string
+  char aux[4]; //chega ate o digito 1000
+  int i = 7, j = 0;
 
   while(entrada[i] != ' '){
     aux[j++] = entrada[i++];
@@ -48,14 +48,14 @@ int** criar(int *alturamax, int *larguramax, char *entrada){
 
 void linha(int **matriz, int alturamax, int larguramax, char *entrada){
   char aux[4]; //chega ate o digito 9999
-  int i = 5, j = 0; // as informações necessárias da entrada começam a partir do indice igual a 5 da string
+  int i = 5, j = 0;
 
   while(entrada[i] != ' '){
     aux[j++] = entrada[i++];
   }
   aux[j] = '\0';
   int x1 = atoi(aux);
-
+  
   if(x1 < 0){
     x1 = 0;
   }
@@ -68,11 +68,11 @@ void linha(int **matriz, int alturamax, int larguramax, char *entrada){
   }
   aux[j] = '\0';
   int y1 = atoi(aux);
-
+  
   if(y1 < 0){
     y1 = 0;
   }
-
+  
   i++;
   j = 0;
 
@@ -81,11 +81,11 @@ void linha(int **matriz, int alturamax, int larguramax, char *entrada){
   }
   aux[j] = '\0';
   int x2 = atoi(aux);
-
+  
   if(x2 > larguramax - 1){
     x2 = larguramax - 1;
   }
-
+  
   i++;
   j = 0;
 
@@ -94,11 +94,11 @@ void linha(int **matriz, int alturamax, int larguramax, char *entrada){
   }
   aux[j] = '\0';
   int y2 = atoi(aux);
-
+  
   if(y2 > alturamax - 1){
     y2 = alturamax - 1;
   }
-
+  
   i++;
   j = 0;
 
@@ -113,11 +113,12 @@ void linha(int **matriz, int alturamax, int larguramax, char *entrada){
       matriz[i][j] = cor;
     }
   }
+  
 }
 
 void circulo(int **matriz, int alturamax, int larguramax, char *entrada){
-  char aux[4]; 
-  int i = 7, j = 0; // as informações necessárias da entrada começam a partir do indice igual a 7 da string 
+  char aux[4];
+  int i = 7, j = 0;
 
   while(entrada[i] != ' '){
     aux[j++] = entrada[i++];
@@ -158,9 +159,9 @@ void circulo(int **matriz, int alturamax, int larguramax, char *entrada){
 
       distancia = sqrt((xcentro-j)*(xcentro-j) + (ycentro-i)*(ycentro-i));
       distancia--;
-      float modulo = sqrt(pow(distancia-raio,2));
+      float modulo = sqrt((distancia - raio) * (distancia - raio));
       
-      if(modulo <= 0.5){     
+      if(modulo <= 0.5){
         matriz[i][j] = cor;
       }
 
@@ -171,7 +172,7 @@ void circulo(int **matriz, int alturamax, int larguramax, char *entrada){
 
 void disco(int **matriz, int alturamax, int larguramax, char *entrada){
   char aux[4];
-  int i = 5, j = 0; // as informações necessárias da entrada começam a partir do indice igual a 5 da string
+  int i = 5, j = 0;
 
   while(entrada[i] != ' '){
     aux[j++] = entrada[i++];
@@ -210,11 +211,10 @@ void disco(int **matriz, int alturamax, int larguramax, char *entrada){
   for(int i = 0; i < alturamax; i++){
     for(int j = 0; j < larguramax; j++){
 
-      distancia = sqrt((xcentro-j)*(xcentro-j) + (ycentro-i)*(ycentro-i));
+      distancia = sqrt((xcentro-j) * (xcentro-j) + (ycentro-i) * (ycentro-i));
       distancia--;
-      float diferenca = distancia - raio;
       
-      if(diferenca <= 0.5){
+      if(distancia - raio <= 0.5){
         matriz[i][j] = cor;
       }
 
@@ -225,7 +225,7 @@ void disco(int **matriz, int alturamax, int larguramax, char *entrada){
 
 void retangulo(int **matriz, int alturamax, int larguramax, char *entrada){
   char aux[4]; //chega ate o digito 9999
-  int i = 5, j = 0; // as informações necessárias da entrada começam a partir do indice igual a 5 da string
+  int i = 5, j = 0;
 
   while(entrada[i] != ' '){
     aux[j++] = entrada[i++];
@@ -269,51 +269,25 @@ void retangulo(int **matriz, int alturamax, int larguramax, char *entrada){
   aux[j] = '\0';
   int cor = atoi(aux);
 
-
-  //determinaçao do limite inferior de y
-  int inferiory;
-  if(altura%2 == 0 && altura != alturamax){
-    inferiory = ycentro - (altura/2 - 1);
-  }else{
-    inferiory = ycentro - altura/2;
-  }
-  if(inferiory < 0){
-    inferiory = 0;
-  }
-   
-  //determinaçao do limite superior de y
-  int superiory = ycentro + altura/2;
-  if(superiory > alturamax - 1){
-    superiory = alturamax - 1;
-  }
-
-  //determinaçao do limite inferior de x
-  int inferiorx;
-  if(largura%2 == 0 && largura != larguramax){
-    inferiorx = xcentro - (largura/2 - 1);
-  }else{
-    inferiorx = xcentro - largura/2;
-  }
-  if(inferiorx < 0){
-    inferiorx = 0;
-  } 
-  //determinaçao do limite superior de x
-  int superiorx = xcentro + largura/2;
-  if(superiorx > larguramax - 1){
-    superiorx = larguramax - 1;
-  }
   
-  
-  for(int i = inferiory; i <= superiory; i++){
-    for(int j = inferiorx; j <= superiorx; j++){
-      matriz[i][j] = cor;
+  for(int i = 0; i < alturamax; i++){
+    for(int j = 0; j < larguramax; j++){
+
+      int moduloi = sqrt((i - ycentro) * (i - ycentro));
+      int moduloj = sqrt((j - xcentro) * (j - xcentro));
+
+      if(moduloi < altura/2 && moduloj < largura/2){
+        matriz[i][j] = cor;
+      }
+
     }
   }
+  
 }
 
 void export(int **matriz, int alturamax, int larguramax, char *entrada){
   char aux[20]; // deve ser o suficiente para pegar o entrada e a extensao do arquivo
-  int i = 7, j = 0; // as informações necessárias da entrada começam a partir do indice igual a 7 da string
+  int i = 7, j = 0;
   FILE *saida;
 
   while(entrada[i] != '\n'){
